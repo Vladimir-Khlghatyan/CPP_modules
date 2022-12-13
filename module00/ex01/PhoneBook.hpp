384bb3c7e7fb205a06ce9a51e6402337f33c4a4d
Vladimir <94677889+Vladimir-Khlghatyan@users.noreply.github.com>
2022-12-13 18:19:02 +0400

    Add files via upload

---
+++ module00/ex01/PhoneBook.hpp
@@ -0,0 +1,38 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include <string>
+# include <iomanip>
+# include <cstdlib>
+# include <cctype>
+# include "Contact.hpp"
+
+# define RED	"\33[1;31m"
+# define GREEN	"\33[1;32m"
+# define PINK	"\33[1;35m"
+# define YELLOW	"\33[1;33m"
+# define BLUE	"\33[1;36m"
+# define CYAN	"\33[1;37m"
+# define WHITE	"\033[1;37m"
+# define RESET	"\33[0;m"
+
+class PhoneBook
+{
+	public:
+
+		PhoneBook();
+		~PhoneBook();
+		void	set_contact(std::string fields[5]);
+		void	print_phonebook() const;
+		void	print_contact(int index) const;
+		int		get_size() const;
+
+	private:
+
+		int		m_size;
+		int		m_index;
+		Contact	m_contact[8];
+};
+
+#endif
