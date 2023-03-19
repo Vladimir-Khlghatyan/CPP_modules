56f69fbf5cce11e5a5cdff5f27314a64bf92ef8b
Vladimir <94677889+Vladimir-Khlghatyan@users.noreply.github.com>
2023-03-20 00:06:52 +0400

    [ADD] init ex00

---
+++ ex01/RPN.hpp
@@ -0,0 +1,40 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <iostream>
+# include <stack>
+# include <algorithm>
+# include <limits>
+
+# define RED    "\33[1;31m"
+# define GREEN  "\33[1;32m"
+# define YELLOW "\33[1;33m"
+# define BLUE   "\33[1;34m"
+# define PINK   "\33[1;35m"
+# define CYAN   "\33[1;36m"
+# define WHITE	"\33[1;37m"
+# define RESET	"\33[0;m"
+
+
+class RPN
+{
+	private:
+      std::stack<double> _stack;
+
+    public:
+      RPN(void);						          // Default constructor
+      RPN(const RPN &other);			    // Copy constructor
+      RPN &operator=(const RPN &rhs);	// Copy assignment operator
+      ~RPN(void);						          // Destructor
+
+    public:
+      int    expressionResult(std::string expression);
+
+    private:
+      void    strTrim(std::string &str);
+      void    removeUnnecessarySpaces(std::string &str);
+      int     checkAllowedSymbols(std::string &str);
+      int     errorMsg(const std::string &msg);
+};
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,20 @@
+#include "RPN.hpp"
+#include <cstring>
+
+int main(int argc, char **argv)
+{
+    std::string expression;
+
+    if (argc != 2)
+    {
+        std::cerr << RED << "Error: the number of arguments must be 1." << RESET << std::endl;
+        return 1;
+    }
+    expression = argv[argc - 1];
+    
+
+    RPN rpn = RPN();
+    rpn.expressionResult(expression);
+
+    return (0); 
+}
