115f374c3a207c1275220162bba7d02ebed77f0e
Vladimir Khlghatyan <vladimirkhlghatyan.m@gmail.com>
2023-03-23 19:23:20 +0400

    [UPDATE] final

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -3,6 +3,7 @@
 int	main(int ac, char **av)
 {
 	std::string numbers;
+	struct timeval tv;
 
 	for (int i = 1; i < ac; ++i)
 	{
@@ -20,31 +21,50 @@ int	main(int ac, char **av)
 		return 1;
 	}
 
-	int printCount = 10;
-	pmm.vectorPrint(printCount);
-	pmm.vectorCheckIfSorted();
-	pmm.listPrint(printCount);
-	pmm.listCheckIfSorted();
+	// print numbers before sorting
+	std::cout << PINK << "Before [std::vecotr]: ";
+	pmm.vectorPrint(10);
+	std::cout << PINK << "Before [std::list]  : ";
+	pmm.listPrint(10);
+	// pmm.vectorCheckIfSorted();
+	// pmm.listCheckIfSorted();
 
-	struct timeval tv;
-    gettimeofday(&tv, nullptr);
+
+    // sorting numbers with std::vector
+	gettimeofday(&tv, nullptr);
     long long vectorStartTime = tv.tv_sec * 1000000LL + tv.tv_usec;
 	pmm.vectorMergeInsertionSort(0, pmm.getVectorSize() - 1);
 	gettimeofday(&tv, nullptr);
     long long vectorEndTime = tv.tv_sec * 1000000LL + tv.tv_usec;
-	std::cout << "Time to process a range of " << pmm.getVectorSize();
-	std::cout << "elements with std::vector : " << vectorEndTime - vectorStartTime;
-	std::cout << " milliseconds" << std::endl;
 
-
-
+	// sorting numbers with std::list
+	gettimeofday(&tv, nullptr);
+    long long listStartTime = tv.tv_sec * 1000000LL + tv.tv_usec;
 	pmm.listMergeInsertionSort(0, pmm.getListSize() - 1);
+	gettimeofday(&tv, nullptr);
+    long long listEndTime = tv.tv_sec * 1000000LL + tv.tv_usec;
 
-	pmm.vectorPrint(printCount);
-	pmm.vectorCheckIfSorted();
-	pmm.listPrint(printCount);
-	pmm.listCheckIfSorted();
+	// print numbers after sorting
+	std::cout << BLUE << "After  [std::vecotr]: ";
+	pmm.vectorPrint(10);
+	std::cout << BLUE << "After  [std::list]  : ";
+	pmm.listPrint(10);
+	// pmm.vectorCheckIfSorted();
+	// pmm.listCheckIfSorted();
 
+	// print messager about sorting duration with std::vector
+	std::cout << GREEN << "Time to process a range of " << pmm.getVectorSize();
+	std::cout << " elements with std::vector : ";
+	std::cout << YELLOW << std::fixed << std::setw(8) << std::setprecision(3);
+	std::cout << (vectorEndTime - vectorStartTime) / 1000.0;
+	std::cout << " milliseconds" << RESET << std::endl;
+
+	// print messager about sorting duration with std::list
+	std::cout << GREEN << "Time to process a range of " << pmm.getListSize();
+	std::cout << " elements with std::list   : ";
+	std::cout << YELLOW << std::fixed << std::setw(8) << std::setprecision(3);
+	std::cout << (listEndTime - listStartTime) / 1000.0;
+	std::cout << " milliseconds" << RESET << std::endl;
 
 	return 0;
 }
